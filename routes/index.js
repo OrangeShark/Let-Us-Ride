5b4d8a426c968e4e9d705a70dc658a9804dd4c09
Erik Edrosa <erik.edrosa@gmail.com>
2013-08-24 19:51:20 -0400

    Added new names and storing carpools in db

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var Datastore = require('nedb');
 
-carpooldb = new Datastore({filename: 'data/carpools.db', autoload: true, nodeWebkitAppName: 'Let-Us-Ride'});
+carpooldb = new Datastore({filename: 'data/carpools.db', autoload: true});
 /*
  * GET home page.
  */
@@ -11,9 +11,13 @@ exports.index = function(req, res){
   carpooldb.find({}, function(err, docs){
     var i,
         carpools = [];
+
+    console.log(docs.length);
     for(i = 0; i < docs.length; i++){
-      carpools[i] = {name: docs[i].name, url: docs[i].url};
+      console.log(JSON.stringify(docs[i]));
+      carpools[i] = {name: docs[i].startLocation + " - " + docs[i].endLocation, url: docs[i].url};
     }
+    data.carpool = carpools;
     res.render('index', data);
   });
 };
@@ -39,11 +43,16 @@ exports.payment = function(req, res){
 };
 
 exports.submitCarpool = function(req, res){
-  
+  console.log(JSON.stringify(req.body.carpool));
+  carpooldb.insert(req.body.carpool, function(err, newDoc){
+    console.error(err);
+    res.end();
+
+  });
 };
 
 exports.makePayment = function(req, res){
-
+  
 };
 
 exports.updateInvitation = function(req, res){
