748e35e261ebf1b02b3c1fe7ce9337a799b0906a
Erik Edrosa <erik.edrosa@gmail.com>
2013-08-24 17:33:46 -0400

    Added database

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
+var Datastore = require('nedb');
 
+carpooldb = new Datastore({filename: 'data/carpools.db', autoload: true, nodeWebkitAppName: 'Let-Us-Ride'});
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
   var data = {title: 'Let Us Ride'};
-  data.carpool = [];
-  res.render('index', data);
+
+  carpooldb.find({}, function(err, docs){
+    var i,
+        carpools = [];
+    for(i = 0; i < docs.length; i++){
+      carpools[i] = {name: docs[i].name, url: docs[i].url};
+    }
+    res.render('index', data);
+  });
 };
 
 exports.create = function(req, res){
